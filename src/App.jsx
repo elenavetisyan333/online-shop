11eb2ca52b2038a1bd41a3f0c955061d1825f60a
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2023-04-19 21:53:38 +0400

    create a structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-// import { useState } from 'react'   
+/* eslint-disable react/react-in-jsx-scope */
+// import { useState } from 'react'  
 import "./App.css";
-import products from "../server";
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import Header from "./Header";
+import Home from "./Home";
+import Basket from "./Basket";
+import { useDispatch } from "react-redux";
+import { setProducts } from "./store/slices/products";
+
+
 function App() {
-  console.log(products);
+  const dispatch = useDispatch();
+
+  async function addProductsToStore(){
+    const myProducts = await axios.get("http://localhost:3000/products").then(rsp => rsp.data);
+
+    dispatch(setProducts(myProducts));
+  }
+
+  useEffect(()=>{
+    addProductsToStore();
+  },[]);
+
+  return (
+    <div className="content">
+      <Header />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/basket' element={<Basket />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as StoreProvider } from "react-redux";
+import store from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <App />
+    <BrowserRouter>
+        <StoreProvider store={store}>
+            <App />
+        </StoreProvider>
+    </BrowserRouter>
 );
